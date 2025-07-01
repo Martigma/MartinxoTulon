be1adca70e679cfa57ad52f95f102cf64ed0d173
Martigma <ma.cervela@duocuc.cl>
2025-07-01 14:54:15 -0400

    Sumar fixed

--- sumar.py
+++ sumar.py
@@ -1,7 +1,11 @@
 def sumar(a ,b):
     return a+b
-
-n1 = int(input("Ingrese valor: "))
-n2 = int(input("Ingrese valor: "))
+while True:
+    try:
+        n1 = int(input("Ingrese valor: "))
+        n2 = int(input("Ingrese valor: "))
+        break
+    except:
+        print("Ingrese solo numeros!")
 
 print(sumar(n1, n2))
